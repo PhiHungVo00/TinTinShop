6b22d1521d04695485aab738fb5e866c991c51ac
TrinhHoa-23520508 <23520508@gm.uit.edu.vn>
2025-05-12 17:18:45 +0700

    init

---
+++ server/TinTin/TinTin/src/main/java/com/example/TinTin/TinTinApplication.java
@@ -0,0 +1,13 @@
+package com.example.TinTin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinTinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinTinApplication.class, args);
+	}
+
+}
---
+++ server/TinTin/TinTin/src/test/java/com/example/TinTin/TinTinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TinTin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinTinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
